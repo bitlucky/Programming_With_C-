c57e3c66d439e3acc1e38ee63b66de20153301d1
bitlucky <lokeshtiwari877@gmail.com>
2019-12-27 22:21:21 +0530

    count length of a linked list

---
+++ 3. countlength.cpp
@@ -0,0 +1,13 @@
+void countlength(struct node *p , *temp )
+{
+	tmp=(struct node *)malloc(sizeof(struct node));
+	p=start;
+	count=0;
+	while(p->link!==NULL)
+	{
+		count++;
+
+	}
+return count;
+}
+
